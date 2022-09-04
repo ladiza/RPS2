5d920b0c7d0ae3f4f0ee680a249474e2e1f063bc
ladiza <ladizala@gmail.com>
2022-09-04 21:59:01 +0200

    Add result decision logic

--- script.js
+++ script.js
@@ -2,15 +2,25 @@
 console.log("This is rock-paper-scissors game, enter your choice and press enter.")
 
 //Prompt user to input "rock", "paper" or "scissors"
-const playerChoice = prompt("Enter your choice: ").toLowerCase
+const playerChoice = prompt("Enter your choice: ").toLowerCase()
 
 //Make random choice as computer
 const options = Array("rock", "paper", "scissors")
 const computerChoice = options[Math.floor(Math.random()*options.length)];
 
 //Show what choosed computer
-console.log("Computer chose ${computerChoice}, you chose ${playerChoice}")
+console.log(`Computer chose ${computerChoice}, you chose ${playerChoice}`)
 
 //Decide who won
-
+if(computerChoice === playerChoice){
+  console.log("It's a draw!")
+} else if (computerChoice === "rock" && playerChoice === "scissors") {
+  console.log("Computer won!")
+} else if (computerChoice === "paper" && playerChoice === "rock") {
+  console.log("Computer won!")
+} else if (computerChoice === "scissors" && playerChoice === "paper") {
+  console.log("Computer won!")
+} else {
+  console.log("You won!")
+}
 
